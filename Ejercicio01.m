69d8a520b086535a5ee4fad757fe2f3666d9726d
Eugenio Salguero <essalguero@yahoo.es>
2018-02-09 00:18:22 +0100

    Ejercicios 1 y 2 hechos en clase

---
+++ Ejercicio01.m
@@ -0,0 +1,66 @@
+close all
+clc
+
+sol = @(t) 2 -exp(-10*t);
+
+f = @(t,y) 10*(2 - y);
+tspan = [0 3];
+h1 = 0.3;
+h2 = 0.15;
+h3 = 0.05;
+y0 = 1;
+
+ 
+
+tspan = [0, 3];
+
+[t1, y1] = feuler(f, tspan, y0, (tspan(2) - tspan(1))/h1);
+[t2, y2] = feuler(f, tspan, y0, (tspan(2) - tspan(1))/h2);
+[t3, y3] = feuler(f, tspan, y0, (tspan(2) - tspan(1))/h3);
+
+
+figure
+plot(t1, y1, t1, sol(t1))
+legend('aprox', 'exacta');
+figure
+plot(t2, y2, t2, sol(t2))
+legend('aprox', 'exacta');
+figure
+plot(t3, y3, t3, sol(t3))
+legend('aprox', 'exacta');
+
+
+h4 = 0.025;
+
+[t4, y4] = beuler(f, tspan, y0, (tspan(2) - tspan(1))/h4);
+figure
+plot(t4, y4, t4, sol(t4))
+legend('aprox', 'exacta');
+[t4, y4](rows(t4),:)
+
+
+[t5, y5] = cranknic(f, tspan, y0, (tspan(2) - tspan(1))/h4);
+figure
+plot(t5, y5, t5, sol(t5))
+legend('aprox', 'exacta');
+[t5', y5](rows(t5'),:)
+
+valorAnalitico = sol(0.3)
+valorBeuler = y4(lookup(t4, 0.3))
+valorCN = y5(lookup(t5, 0.3))
+
+errorBeuler = abs(valorAnalitico - valorBeuler)
+errorCN = abs(valorAnalitico - valorCN)
+
+if errorBeuler < errorCN
+  disp("Beuler mas precios")
+else
+  disp("CN mas preciso")
+end
+
+errores = abs(y4 .- y5);
+
+figure
+plot(t5, errores)
+title("Error")
+legend("Error beuler/cranknic")
---
+++ Ejercicio02.m
@@ -0,0 +1,92 @@
+clear all
+clc
+
+format long
+
+sol = @(t) exp(((1/2) * t .^ 2) - t) ./ (1 + exp(((1/2) * t .^ 2) - t))
+
+f = @(t, y) (t - 1).*(y - y.^2)
+y0 = 0.5
+tspan = [0 2];
+h1 = 0.004;
+
+[t1, y1] = rk2(f, tspan, y0, (tspan(2) - tspan(1))/h1);
+[t2, y2] = rk3(f, tspan, y0, (tspan(2) - tspan(1))/h1);
+
+y1(rows(t1))
+y2(rows(t2))
+
+valorAnalitico = sol(1.24)
+valorRk2 = y1(lookup(t1, 1.24))
+valorRk3 = y2(lookup(t2, 1.24))
+
+errorRk2 = abs(valorAnalitico - valorRk2)
+errorRk3 = abs(valorAnalitico - valorRk3)
+
+if errorRk2 < errorRk3
+  disp("errorRk2 mas precios")
+else
+  disp("errorRk3 mas preciso")
+end
+
+
+
+%figure
+%plot(t1, errorRk2)
+%title("Error")
+%legend("Error errorRk2")
+%
+%figure
+%plot(t2, errorRk3)
+%title("Error")
+%legend("Error errorRk3")
+
+%Calcular las soluciones exactas
+soluciones = zeros(rows(t1), 2);
+for i = 1:rows(t1)
+  soluciones(i,:) = [t1(i), sol(t1(i), 1)];
+end
+
+soluciones(rows(t1))
+
+erroresRk2 = abs(y1 .- soluciones(:,2));
+
+erroresRk3 = abs(y2 .- soluciones(:,2));
+
+figure
+plot(t1, erroresRk2)
+title("Error")
+legend("Error errorRk2")
+
+figure
+plot(t2, erroresRk3)
+title("Error")
+legend("Error errorRk3")
+
+tablaTiempos04 = find(soluciones(:,2) < 0.4);
+soluciones(min(tablaTiempos04), 1)
+soluciones(max(tablaTiempos04), 1)
+
+
+I = [0:10^-6:2];
+soluciones = sol(I);
+minimo = min(find(soluciones < 0.4)) * 10^-6
+maximo = max(find(soluciones < 0.4)) * 10^-6
+
+figure
+plot(I, sol(I))
+title("Funcion original")
+legend("Sol")
+
+
+minimoRK3 = min(find(y2 < 0.4)) * h1
+maximoRK3 = max(find(y2 < 0.4)) * h1
+
+figure
+plot(I, sol(I))
+title("Funcion RK3")
+legend("RK3")
+
+
+
+% bisection
